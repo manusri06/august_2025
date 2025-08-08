bb0cdfc93ff4cb3678d795ade4f5b43831a30560
V. Manusri <velpula.manusri06@gmail.com>
2025-08-08 17:18:58 +0530

    Time: 0 ms (100%), Space: 12.8 MB (20.64%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,22 @@
 class Solution(object):
     def subsets(self, nums):
-        n = len(nums)
-        ans = []
-        for i in range(1<<n):
-            subset = []
-            for j in range(n):
-                if i & (1<<j):
-                    subset.append(nums[j])
-            ans.append(subset)
-        return ans
+        def rec(index):
+            if index == len(nums):
+                return [[]]
+            s = rec(index+1)
+            s1 = []
+            for subset in s:
+                s1.append([nums[index]]+subset)
+            return s+s1
+        return rec(0)
+
+'''n = len(nums)
+ans = []
+for i in range(1<<n):
+    subset = []
+    for j in range(n):
+        if i & (1<<j):
+            subset.append(nums[j])
+    ans.append(subset)
+return ans'''
         
