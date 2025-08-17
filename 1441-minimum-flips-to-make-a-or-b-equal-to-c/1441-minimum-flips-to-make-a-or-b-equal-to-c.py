c21f833c8e5585c0ca97ba1c58d5b66b582b9675
V. Manusri <velpula.manusri06@gmail.com>
2025-08-17 12:51:21 +0530

    Time: 36 ms (69.15%), Space: 17.8 MB (51.37%) - LeetHub

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.py
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/1441-minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -3,17 +3,14 @@ class Solution:
         
         count = 0
         while c > 0 or a>0 or b>0:
-            if c&1:
-                if not((a&1) | (b&1)):
-                    count+=1
-            else:
-                if (a&1) & (b&1):
-                    count += 2
-                elif (a&1) | (b&1):
-                    count+=1
-            c = c>> 1
-            a = a >> 1
-            b = b >> 1
+            if c&1 != (a&1 | b&1):
+                if c&1 == 1:
+                    count += 1
+                else:
+                    count += (a&1)+(b&1)
+            c = c>>1
+            a = a>>1
+            b = b>>1
         return count
 
 
