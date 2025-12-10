ea4302522c85e1e754225fe92224ff895e747683
V. Manusri <velpula.manusri06@gmail.com>
2025-12-10 14:12:25 +0530

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict={}
+        dict={} 
         for i,n in enumerate(nums):
             diff = target - n
             if diff in dict:
